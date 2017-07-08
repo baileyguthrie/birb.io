c71e0f6b5e861fe40fd2786752ee6485c7873f9b
Bailey Guthrie <baileynicholasguthrie@gmail.com>
2017-07-08 01:39:36 +0000

    Completed style for search page

--- webapp/static/js/script.js
+++ webapp/static/js/script.js
@@ -1,21 +1,46 @@
 $(document).ready(function() {
     
-   $('.search-row').keyup(function(event) {
-       var query = $('#query').val();
-       if ($.trim(query)) {
+    $('.search-row').keyup(function(event) {
+        var query = $('#query').val();
+        if ($.trim(query)) {
             $('#submit-btn').prop('disabled', false);
-       } else {
+        } else {
             $('#submit-btn').prop('disabled', true);
-       }
-   });
+        }
+    });
    
-   $('.footer').click(function() {
-      if($('.footer').hasClass('slide-up')) {
-        $('.footer').addClass('slide-down', 1000, 'easeOutBounce');
-        $('.footer').removeClass('slide-up'); 
-      } else {
-        $('.footer').removeClass('slide-down');
-        $('.footer').addClass('slide-up', 1000, 'easeOutBounce'); 
-      }
-  });
+    if (window.location.pathname == "/") {
+        $('#search').addClass('active-page');
+    }
+    
+    if (window.location.pathname == "/compare") {
+        $('#compare').addClass('active-page');
+    }
+    
+    if (window.location.pathname == "/trending") {
+        $('#trending').addClass('active-page');
+    }
+    
+    if ($('.results-statement:contains("Positive")').length) {
+        $('.results-well').addClass('positive-well');
+    } else if ($('.results-statement:contains("Negative")').length) {
+        $('.results-well').addClass('negative-well');
+    }
+    
+    
+    
+    
+    
+    $('#collapseOne').on('show.bs.collapse', function () {    
+        $('.panel-heading').animate({
+            backgroundColor: "#515151"
+        }, 500);   
+    })
+    
+    $('#collapseOne').on('hide.bs.collapse', function () {    
+        $('.panel-heading').animate({
+            backgroundColor: "#00B4FF"
+        }, 500);   
+    })
+    
 });
