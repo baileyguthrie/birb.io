b65ebe3d87f0953c0af409da4e847800dc962e2a
Bailey Guthrie <baileynicholasguthrie@gmail.com>
2017-07-20 21:48:08 +0000

    Production

--- django_workspace/wsgi.py
+++ django_workspace/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prompt.settings")
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_workspace.settings")
